69b583a287e49b285bc6523204dbbb461ea76eec
Tillwege <tillwege@gmail.com>
2022-11-17 15:21:36 +0100

    singleplayer done

--- components/tile.tsx
+++ components/tile.tsx
@@ -1,16 +1,45 @@
 import Image from "next/image";
+import React from "react";
+import { useState } from "react";
 import styles from "../styles/tile.module.css";
 
-interface tileprops {
+export interface tileprops {
+  card_index: number;
   image_index: number;
+  hidden: boolean;
+  collected: boolean;
+  onClick: (key: number, id: number) => void;
 }
+
 export default function Tile(props: tileprops) {
+  let style;
+  if (props.collected) {
+    style = styles.collected;
+  } else if (props.hidden) {
+    style = styles.item_hidden;
+  } else {
+    style = styles.item_show;
+  }
+
   return (
     <div
-      className={styles.item}
+      className={style}
       style={{ width: "100%", height: "100%", position: "relative" }}
+      onClick={() => {
+        if (!props.collected) {
+          props.onClick(props.card_index, props.image_index);
+        }
+      }}
     >
-      <Image alt="A aicture of a cat" src={`/${props.image_index}.png`} fill />
+      {!props.hidden && (
+        <Image
+          alt="A aicture of a cat"
+          src={`/${props.image_index}.png`}
+          fill
+          sizes="100% 100%"
+          className="image"
+        />
+      )}
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,85 @@
 import Head from "next/head";
-import { ReactElement } from "react";
-import Tile from "../components/tile";
+import { useState } from "react";
 
-const image_count: Number = 18;
+import Tile, { tileprops } from "../components/tile";
+
+const image_count: number = 18;
+
+interface card_data {
+  card_index: number;
+  image_index: number;
+}
 
 export default function Home() {
+  let ids = [...Array(image_count).keys()];
+  ids = shuffle(ids.concat([...Array(image_count).keys()]));
+  let [openCards, setOpenCards] = useState(Array());
+  let [collectedCards, setCollectedCards] = useState(Array());
+
+  let key = 0;
+  let [tiles, SetTiles] = useState(
+    ids.map((id) => {
+      let props: card_data = {
+        card_index: key,
+        image_index: id,
+      };
+      key++;
+      return props;
+    })
+  );
+
+  function cardClicked(card_index: number, id: number) {
+    if (openCards.length == 2 || openCards.includes(card_index)) {
+      if (
+        getImageIndexByCardIndex(openCards[0]) ==
+        getImageIndexByCardIndex(openCards[1])
+      ) {
+        let oldCollectedCards = [...collectedCards];
+        oldCollectedCards.push(openCards[0]);
+        oldCollectedCards.push(openCards[1]);
+        setCollectedCards(oldCollectedCards);
+      }
+      setOpenCards(Array());
+    } else {
+      let oldOpenCards = [...openCards];
+      oldOpenCards.push(card_index);
+      setOpenCards(oldOpenCards);
+    }
+  }
+
+  function getImageIndexByCardIndex(card_index: number): number {
+    let res = -1;
+    tiles.forEach((tile) => {
+      if (card_index == tile.card_index) {
+        res = tile.image_index;
+      }
+    });
+    return res;
+  }
+
   return (
     <div>
       <Head>
         <title>Caros Cat Memory</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      {create_grid()}
-    </div>
-  );
-}
+      <h1>Caros cat Memory</h1>
+      <h1>Your turn</h1>
 
-function create_grid(): ReactElement {
-  let ids = [...Array(image_count).keys()];
-  let ids2 = (ids = ids.concat([...Array(image_count).keys()]));
-  ids = shuffle(ids);
-
-  return (
-    <div className="container">
-      {ids.map((id) => {
-        return <Tile key={id} image_index={id}></Tile>;
-      })}
+      <div className="container">
+        {tiles.map((data) => {
+          return (
+            <Tile
+              hidden={!openCards.includes(data.card_index)}
+              collected={collectedCards.includes(data.card_index)}
+              image_index={data.image_index}
+              onClick={cardClicked}
+              card_index={data.card_index}
+              key={data.card_index}
+            ></Tile>
+          );
+        })}
+      </div>
     </div>
   );
 }
